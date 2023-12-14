6352a224a422b7f4fe602a0fcdb6c8c09c09d2bf
Rutvik Gujarati <mrrutvikgujarati0007@gmail.com>
2023-12-14 14:49:03 +0530

    encoding and fallback

--- src/Counter.sol
+++ src/Counter.sol
@@ -3,12 +3,34 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     uint256 public number;
+    uint256 public store;
+    uint256 public constant value= 4;
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
     }
 
-    function increment() public {
+    function increment() public {   
         number++;
+    }
+
+    //falback and recieve
+
+    fallback() external payable{}
+
+    receive() external payable{}
+
+    //abi encoding
+
+    function Encoding()public pure returns(bytes memory){
+        bytes memory num = abi.encode("hii");
+        return num;
+    }
+
+    //encoding with small space require for bytes .. 
+
+    function packed()public pure returns(bytes memory){
+        bytes memory string1 = abi.encodePacked("this is what we want");
+        return string1;
     }
 }
