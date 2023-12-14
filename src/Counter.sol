81dc127d5244922b483c36a96478fc4f3f56ac17
Rutvik Gujarati <mrrutvikgujarati0007@gmail.com>
2023-12-14 15:13:23 +0530

    encode-decode

--- src/Counter.sol
+++ src/Counter.sol
@@ -23,7 +23,7 @@ contract Counter {
     //abi encoding
 
     function Encoding()public pure returns(bytes memory){
-        bytes memory num = abi.encode("hii");
+        bytes memory num = abi.encode("this is what we want");
         return num;
     }
 
@@ -33,4 +33,30 @@ contract Counter {
         bytes memory string1 = abi.encodePacked("this is what we want");
         return string1;
     }
+    //decoding with using funciton
+
+    function encod() public  pure  returns(string memory){
+          string memory name = abi.decode(Encoding(), (string));
+        return  name;
+    }
+
+    //decoding with bytes 
+    // function decodewithByte()public  pure  returns(string memory){
+    //     string memory nmae2 = abi.decode((0x7468697320697320776861742077652077616e74), (string));
+    //     return  nmae2;  //this is not possible
+    // }
+
+    //multi encode and decode
+    function multiEncode ()public  pure  returns(bytes memory){
+        (bytes memory string1)= abi.encode("this is hilarious","too much hilarious"); 
+        return (string1);
+    }
+
+    //multi decode 
+
+    function multidecode()public  pure returns(string memory ,string memory){
+        (string memory newOne, string memory oldOne) = abi.decode(multiEncode(), (string,string));
+        return  (newOne, oldOne);
+    }
+
 }
