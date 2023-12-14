cc705b87607b356eec43c74ca8c438cfcebf8300
Rutvik Gujarati <mrrutvikgujarati0007@gmail.com>
2023-12-14 12:06:47 +0530

    setup

---
+++ src/NFT.sol
@@ -0,0 +1,9 @@
+//SPDX-Licence-Identifier : MIT
+
+pragma solidity ^0.8.13;
+
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract NFT is ERC20 {
+    constructor() ERC20("newNFT", "NFt") {}
+}
